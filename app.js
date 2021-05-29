4066fcbdeb9a30a25bcd94052d708b22fbda5a93
anubhavsaraswat <mailanubhavsaraswat@gmail.com>
2021-05-30 04:02:42 +0530

    input | output processing Done

---
+++ app.js
@@ -0,0 +1,12 @@
+var btnId=document.querySelector("#btn");
+var txt = document.querySelector("#mytext");
+var out= document.querySelector(".output");
+
+function execute(){
+   out.innerText="pika pikap pp pp "
+};
+
+
+btnId.addEventListener("click",execute)
+
+
